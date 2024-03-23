5dbe0e628a3cff84632ca4b95ca01b1ee828d7ba
Hanna Karabankova <Annakarabankova98@gmail.com>
2024-03-23 20:12:13 +0300

    update React.memo in the ChildComponent

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,26 +1,31 @@
-import { memo, useCallback, useMemo } from 'react';
+import { memo } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-const List = ({ value, onItemChange }) => {
-    const addValue = useCallback(
-        (index) => {
-            const newData = [...value];
-            newData[index] = '!!!' + newData[index];
-            onItemChange(newData);
-        },
-        [onItemChange, value],
+function arePropsEqual(oldProps, newProps) {
+    return (
+        oldProps.value.length === newProps.value.length &&
+        oldProps.value.every((oldData, index) => {
+            const newData = newProps.value[index];
+            return oldData === newData;
+        })
     );
+}
 
-    let finalItems = useMemo(() => {
-        return value.map((item, index) => (
-            <li key={uuidv4()}>
-                <button onClick={() => addValue(index)}>Add !!!</button>
-                {item}
-            </li>
-        ));
-    }, [addValue, value]);
+const List = ({ value, onItemChange }) => {
+    const addValue = (index) => {
+        const newData = [...value];
+        newData[index] = '!!!' + newData[index];
+        onItemChange(newData);
+    };
+
+    let finalItems = value.map((item, index) => (
+        <li key={uuidv4()}>
+            <button onClick={() => addValue(index)}>Add !!!</button>
+            {item}
+        </li>
+    ));
 
     return <ul>{finalItems}</ul>;
 };
 
-export default memo(List);
+export default memo(List, arePropsEqual);
