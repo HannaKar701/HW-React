9c0d5d44c375fcc3b876ebb189bf652bb568f907
Hanna Karabankova <Annakarabankova98@gmail.com>
2024-03-24 21:32:22 +0300

    add night mode with styled components library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { useState, useRef } from 'react';
 import ChildComponent from './components/ChildComponent.jsx';
+import { GlobalStyles } from './components/GlobalStyles';
+import { lightTheme, darkTheme } from './components/Themes';
+import { ThemeProvider } from 'styled-components';
+import { useDarkMode } from './components/useDarkMode';
+import Toggle from './components/Toggler';
 
 import './App.css';
 
 const App = () => {
+    const [theme, themeToggler] = useDarkMode();
+    const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
     const [arrList, setArrList] = useState([1, 2]);
     const link = useRef(null);
     const addFocus = () => link.current.focus();
@@ -14,11 +22,16 @@ const App = () => {
     };
 
     return (
-        <>
-            <input ref={link} onKeyDown={addItem}></input>
-            <button onClick={addFocus}>Add focus</button>
-            <ChildComponent value={arrList} onItemChange={setArrList} />
-        </>
+        <ThemeProvider theme={themeMode}>
+            <>
+                <GlobalStyles />
+                <input ref={link} onKeyDown={addItem}></input>
+                <button onClick={addFocus}>Add focus</button>
+                <ChildComponent value={arrList} onItemChange={setArrList} />
+                <br />
+                <Toggle theme={theme} toggleTheme={themeToggler} />
+            </>
+        </ThemeProvider>
     );
 };
 
